f2e4fb3aa3c5497c2612392cf020b59536d77dbf
benjmng <benjmng@gmail.com>
2023-05-25 14:18:03 +0200

    test Index

---
+++ design/modules/async_image.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt = "", widths, sizes) {
+
+  src = './contenu/_media/' + src
+  console.log(widths)
+  let metadata = await Image(src, {
+    widths: widths || ["auto"],
+    formats: ["avif", "webp"],
+    outputDir: "./public/_media/",
+    urlPath: "./media/",
+    filenameFormat: function (id, src, width, format, options) {
+        const extension = path.extname(src)
+        const name = path.basename(src, extension)
+        return `${name}-${width}w.${format}`
+      }
+
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on a missing alt (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+};
+
+module.exports = imageShortcode
