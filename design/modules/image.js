a2c18ac6e1b717fbb9333b96ec4f954352c41da2
benjmng <benjmng@gmail.com>
2023-05-28 15:33:55 +0200

    [OTO] Update

--- design/modules/image.js
+++ design/modules/image.js
@@ -4,13 +4,12 @@ const Image = require("@11ty/eleventy-img");
 function imageShortcode(src, alt = "", widths, sizes) {
   
   src = './contenu/' + src
-  let baseURL = process.env.NODE_ENV  == 'production' ? 'https://benjmng.github.io/guez_caraibes/' : './'
 
   let options = {
     widths: widths || ["auto"],
     formats: ["avif", "webp"],
     outputDir: "./public/_media/",
-    urlPath: baseURL + "_media/",
+    urlPath: "/_media/",
     filenameFormat: function (id, src, width, format, options) {
       const extension = path.extname(src)
       const name = path.basename(src, extension)
