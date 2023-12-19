0083f98ce7d881a30e45b73148082587f373f14e
benjmng <benjmng@gmail.com>
2023-12-19 18:21:56 +0100

    Chang CNAME

--- contenu/_data/site.js
+++ contenu/_data/site.js
@@ -1,4 +1,4 @@
 module.exports = {
-  url: process.env.NODE_ENV === 'development' ? '' : 'https://dev2.benjmng.eu',
+  url: process.env.NODE_ENV === 'development' ? '' : 'https://dev2.bnjm.eu',
   currentYear() {const today = new Date();return today.getFullYear();}
 };
